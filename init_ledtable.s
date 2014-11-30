340e14c13fd4ac11164e003b44f46ca61e685fa3
Brian Silvey <blujay270@gmail.com>
2014-11-29 21:55:56 -0500

    added 4x4 led display support

---
+++ init_ledtable.s
@@ -0,0 +1,185 @@
+    AREA asm_func, CODE, READONLY
+    EXPORT init_ledtable
+init_ledtable
+    MOV R1, #0
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #62815
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #64959
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63897
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63894
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #64409
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #62801
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63900
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #61999
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #40953
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #22935
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63209
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3,#63624
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63039
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #62031
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63903
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #62807
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63967
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #62939
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #48093
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #8177
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #63631
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #30855
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #64623
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #38505
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #16099
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #56763
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #43512
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #43354
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #39359
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #12847
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #48605
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #60892
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #6483
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #32190
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #30047
+    STR R3, [R2]
+    
+    ADD R1, R1, #1
+    ADD R2, R0, R1, LSL#2
+    MOV R3, #27030
+    STR R3, [R2]
+   
+    BX  LR
+    END
